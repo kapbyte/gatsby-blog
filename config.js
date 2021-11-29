21bc12289aab59e6c035246f554e06b3f7bd3228
ke1echi <mrkelechichinaka@gmail.com>
2021-11-29 01:23:17 +0100

    first commit

---
+++ config.js
@@ -0,0 +1,50 @@
+'use strict';
+
+module.exports = {
+  url: 'https://lumen.netlify.com',
+  pathPrefix: '/',
+  title: 'Blog by Kelechi Chinaka',
+  subtitle: 'Enjoyment Minister',
+  copyright: '© All rights reserved.',
+  disqusShortname: '',
+  postsPerPage: 4,
+  googleAnalyticsId: 'UA-73379983-2',
+  useKatex: false,
+  menu: [
+    {
+      label: 'Articles',
+      path: '/'
+    },
+    {
+      label: 'About me',
+      path: '/pages/about'
+    },
+    {
+      label: 'Contact me',
+      path: '/pages/contacts'
+    }
+  ],
+  author: {
+    name: 'Kelechi Chinaka',
+    photo: '/photo.jpg',
+    bio: 'Enjoyment version chap.',
+    contacts: {
+      email: '',
+      facebook: '#',
+      telegram: '#',
+      twitter: '#',
+      github: '#',
+      rss: '',
+      vkontakte: '',
+      linkedin: '#',
+      instagram: '#',
+      line: '',
+      gitlab: '',
+      weibo: '',
+      codepen: '',
+      youtube: '',
+      soundcloud: '',
+      medium: '',
+    }
+  }
+};
